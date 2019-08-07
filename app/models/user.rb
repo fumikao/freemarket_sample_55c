fe84e6f6c77ea0290ede812e7aad3ba815cbf590
junki-nishio <thun243@yahoo.co.jp>
2019-08-08 00:15:57 +0900

    omniauth-rails_csrf_protectionの導入。それにあたっての修正。 credential環境での情報修正。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable,
            :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
-  
+           
     has_one :address, dependent: :destroy
     has_one :sns_credential, dependent: :destroy
     accepts_nested_attributes_for :address
